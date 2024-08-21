6fb1adfed742c1bad14697d9efd65270ffdf091f
adorahelen <adorahelenmin@gmail.com>
2024-08-22 01:07:19 +0900

    베이스 설정(테스트+메인 동시에)

--- src/main/java/edu/example/restz/RestzApplication.java
+++ src/main/java/edu/example/restz/RestzApplication.java
@@ -2,10 +2,12 @@ package edu.example.restz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class RestzApplication {
+@ComponentScan(basePackages = {"edu.example.restz", "edu.example.sample"})
 
+public class RestzApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestzApplication.class, args);
 	}
