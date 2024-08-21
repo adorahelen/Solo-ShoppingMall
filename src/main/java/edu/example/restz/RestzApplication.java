98df3cc2c4024e7bd7b594bb0ad75cef1ee8fb7a
adorahelen <adorahelenmin@gmail.com>
2024-08-21 14:36:08 +0900

    Initial commit

---
+++ src/main/java/edu/example/restz/RestzApplication.java
@@ -0,0 +1,13 @@
+package edu.example.restz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestzApplication.class, args);
+	}
+
+}
