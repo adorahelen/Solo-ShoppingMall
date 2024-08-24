e8936b96b47dd18615ebc2cbc04e64c014cc98ff
adorahelen <adorahelenmin@gmail.com>
2024-08-24 19:56:33 +0900

    예외처리 (서비스 단에서 진행)

---
+++ src/main/java/edu/example/restz/exception/EntityNotFoundException.java
@@ -0,0 +1,19 @@
+package edu.example.restz.exception;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class EntityNotFoundException extends RuntimeException {
+    private String message;
+    private int code;
+
+
+    public EntityNotFoundException(String message) {
+        super(message);
+        this.message = message;
+        this.code = 404;
+    }
+}
+
