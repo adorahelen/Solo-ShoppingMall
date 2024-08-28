cdde17640c40ebd9bde420eda48e491906a7dec0
adorahelen <adorahelenmin@gmail.com>
2024-08-29 01:02:19 +0900

    예외처리 : 크기제한, 미업로드

---
+++ src/main/java/edu/example/restz/controller/advice/FileControllerAdvice.java
@@ -0,0 +1,27 @@
+package edu.example.restz.controller.advice;
+
+import edu.example.restz.exception.UploadNotSupportedException;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import java.util.Map;
+
+@RestControllerAdvice
+@Log4j2
+public class FileControllerAdvice {
+
+    @ExceptionHandler(UploadNotSupportedException.class)
+    public ResponseEntity<?> handleException(UploadNotSupportedException e) {
+        return ResponseEntity.badRequest().body(Map.of("error", e.getMessage()));
+    }
+
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<?> handleMaxUploadSizeExceededException(MaxUploadSizeExceededException e) {
+        log.info("파일 크기 제한 초과 " + e.getMessage());
+        return ResponseEntity.badRequest()
+                .body(Map.of("error", "파일 크기 제한 초과 "));
+    }
+}
