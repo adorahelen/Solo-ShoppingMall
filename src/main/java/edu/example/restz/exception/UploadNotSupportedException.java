6b3e19c570cf68067c5d80fd78f430ff2611570f
adorahelen <adorahelenmin@gmail.com>
2024-08-29 01:06:45 +0900

    예외-크기제한, 확장자제한

--- src/main/java/edu/example/restz/exception/UploadNotSupportedException.java
+++
@@ -1,7 +0,0 @@
-package edu.example.restz.exception;
-
-public class UploadNotSupportedException extends RuntimeException {
-    public UploadNotSupportedException(String message) {
-        super(message);
-    }
-}
