8e2d06f2420876a5085c7e2050e261efbfd77e15
adorahelen <adorahelenmin@gmail.com>
2024-08-30 12:39:23 +0900

    select: 지연로딩,즉시로딩+조인방식

--- src/main/java/edu/example/restz/repository/ProductRepository.java
+++ src/main/java/edu/example/restz/repository/ProductRepository.java
@@ -1,7 +1,20 @@
 package edu.example.restz.repository;
 
 import edu.example.restz.entity.Product;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+//    @EntityGraph(attributePaths = {"images"},
+//    type= EntityGraph.EntityGraphType.FETCH)
+//    @Query("SELECT p FROM  Product p WHERE p.pno = :pno")
+//    Optional<Product> getProduct(@Param("pno")Long pno);
+
+    @Query("SELECT p FROM  Product p JOIN FETCH p.images pi WHERE p.pno = :pno")
+    Optional<Product> getProduct(@Param("pno")Long pno);
 }
--- src/test/java/edu/example/restz/repository/ProductRepositoryTests.java
+++ src/test/java/edu/example/restz/repository/ProductRepositoryTests.java
@@ -1,6 +1,7 @@
 package edu.example.restz.repository;
 
 import edu.example.restz.entity.Product;
+import edu.example.restz.entity.ProductImage;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+import java.util.SortedSet;
 import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -43,5 +46,42 @@ public class ProductRepositoryTests {
                     assertEquals(i, saveProduct.getPno());
                     assertEquals(0, saveProduct.getImages().first().getIno());
         });
+    }
+
+
+    // ====== 디폴트값이 지연로딩으로 되어있다. (즉시로딩은 계속 전부 읽어 오기에, @ElementCollection(fetch = FetchType.LAZY))
+    // 따라서 아래와 같이 트랜잭션 리드 온리를 통해 2번 읽어오게 할 수 있고
+    // 아래 아래와 같이 레포인터에 메소드를 하나 정의해서 필요한 것만 즉시 로딩을 통해 조인을 통해 가져오게 할 수 있다.
+    @Test
+    @Transactional(readOnly = true)
+    public void testRead() {
+        Long pno = 1L;
+
+        Optional<Product> foundproduct = productRepository.findById(pno);
+        assertTrue(foundproduct.isPresent(), "Product not found, 상품이 있어야 됩니다.");
+
+        System.out.println("===============================");
+        Product product = foundproduct.get(); // 전부
+        SortedSet<ProductImage> productImages = product.getImages(); // 1. 객체에서 필요한 부분만
+        assertNotNull(productImages); // 2. 널이 아닌지 검증 -> pass
+        assertEquals(0, productImages.first().getIno()); // 3. 첫번째 first
+        System.out.println(productImages);
+
+    }
+
+    // @EntityGraph 쓰는 방법이 있고, 일반 쿼리문으로 Join 연산을 수행도 가능
+    @Test
+    public void testRead2(){
+        Long pno = 1L;
+        Optional<Product> foundproduct = productRepository.getProduct(pno);
+        assertTrue(foundproduct.isPresent());
+
+        Product product = foundproduct.get();
+        SortedSet<ProductImage> productImages = product.getImages();
+        assertNotNull(productImages);
+        assertEquals(0, productImages.first().getIno());
+        System.out.println(productImages);
+
+
     }
 }
