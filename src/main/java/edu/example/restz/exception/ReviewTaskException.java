cba5ef06d926589475f36dd27f3177399f3b387b
adorahelen <adorahelenmin@gmail.com>
2024-09-03 15:24:56 +0900

    리뷰 예외 처리

---
+++ src/main/java/edu/example/restz/exception/ReviewTaskException.java
@@ -0,0 +1,16 @@
+package edu.example.restz.exception;
+
+import lombok.AllArgsConstructor;
+
+import lombok.Getter;
+
+
+@Getter
+@AllArgsConstructor
+public class ReviewTaskException extends RuntimeException {
+
+    private String message;
+    private int code;
+
+
+}
