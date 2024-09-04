bf553e577fc3fd9a9014d1f6c10b7d5d0ea4c144
adorahelen <adorahelenmin@gmail.com>
2024-09-04 21:40:29 +0900

    E->R->DTO->S->C : 생성,수정,삭제DTO

--- src/main/java/edu/example/restz/dto/CartItemDTO.java
+++ src/main/java/edu/example/restz/dto/CartItemDTO.java
@@ -1,4 +1,20 @@
 package edu.example.restz.dto;
 
-public class CartItemDTO {
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class CartItemDTO {// 서비스, 컨트롤러(뷰-모델 || RestAPi)
+
+    private Long itemNo;
+    private Long pno;
+
+    private String pname;
+
+    private int quantity;
+    private int price;
+
+    private String image;
 }
